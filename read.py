0c82a0bc5d7b7661d42e1c13b723038c6ad1f11a
ankle7766 <77894340+ankle7766@users.noreply.github.com>
2021-03-05 15:18:33 +0800

    import progressbar
    try to import a package, and know how to use this.

--- read.py
+++ read.py
@@ -1,11 +1,16 @@
+import time
+import progressbar # 2、6、11行，是此套件的程式碼
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 10000 == 0:
-            print(len(data))
+        bar.update(count)
+        # if count % 10000 == 0:
+        #     print(len(data))
 
 print('檔案讀取玩了，總共有',len(data),'筆資料。')
 
